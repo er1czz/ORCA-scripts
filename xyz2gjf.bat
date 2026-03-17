b32bc983da7e45caeb181c73a205a7ed6d7b9622
ER1C ZZ <38876045+er1czz@users.noreply.github.com>
2026-03-17 21:26:34 +0800

    Update xyz2gjf.bat

--- xyz2gjf.bat
+++ xyz2gjf.bat
@@ -1,5 +1,6 @@
 @echo off
 REM This script copies all files with the .xyz extension as .gjf files in the same directory.
+REM Created by Eric Z. 3/17/2026
 for %%f in (*.xyz) do  (
     echo Processing %%f...
     REM write specific gjf filler
@@ -15,5 +16,4 @@ for %%f in (*.xyz) do  (
     more +2 "%%f" >> "%%~nf.gjf"
 )
 echo Copy process complete.
-echo Created by Eric Z. 3/17/2026
 pause
